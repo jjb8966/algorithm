f7d6ab2b71a0a71b6bb4a2ba8debac36b1d91d3d
jjb8966 <jjb8966@naver.com>
2022-05-31 21:58:33 +0900

    style: input, process, output 메소드 사용

--- BOJ/tree/Main_11725.java
+++ BOJ/tree/Main_11725.java
@@ -7,52 +7,62 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main_11725 {
-    private static ArrayList<Integer>[] adjacencyList;
-    private static int countOfNode;
-    private static int[] parentNode;
 
-    public static void input() throws IOException {
+    private static int numberOfNode;
+    private static int[] parent;
+    private static ArrayList<Integer>[] adjacencyList;
+
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        int vertex1;
+        int vertex2;
 
-        countOfNode = Integer.parseInt(br.readLine());
+        numberOfNode = Integer.parseInt(br.readLine());
 
-        adjacencyList = new ArrayList[countOfNode + 1];     // 노드의 인덱스 : 1 ~ countOfNode
-        parentNode = new int[countOfNode + 1];
+        adjacencyList = new ArrayList[numberOfNode + 1];
+        parent = new int[numberOfNode + 1];
 
-        for (int i = 1; i <= countOfNode; i++) {
+        for (int i = 1; i <= numberOfNode; i++) {
             adjacencyList[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < countOfNode - 1; i++) {         // 트리에서 간선의 수 -> E = V - 1
+        for (int i = 0; i < numberOfNode - 1; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int vertex1 = Integer.parseInt(st.nextToken());
-            int vertex2 = Integer.parseInt(st.nextToken());
+            vertex1 = Integer.parseInt(st.nextToken());
+            vertex2 = Integer.parseInt(st.nextToken());
 
             adjacencyList[vertex1].add(vertex2);
             adjacencyList[vertex2].add(vertex1);
         }
     }
 
-    public static void dfs(int vertex, int parent) {    // dfs로 모든 정점의 부모 노드 구함
-        parentNode[vertex] = parent;
+    private static void process() {
+        dfs(1, -1);
+    }
 
-        for (int temp : adjacencyList[vertex]) {        // vertex의 모든 child에 대해 dfs
-            if (temp == parent) {
+    private static void dfs(int node, int myParent) {
+        parent[node] = myParent;
+
+        for (Integer child : adjacencyList[node]) {
+            if (child == myParent) {
                 continue;
             }
 
-            dfs(temp, vertex);
+            dfs(child, node);
+        }
+    }
+
+    private static void output() {
+        for (int i = 2; i <= numberOfNode; i++) {
+            System.out.println(parent[i]);
         }
     }
 
     public static void main(String[] args) throws IOException {
         input();
-        dfs(1, -1);
-
-        for (int i = 2; i < countOfNode + 1; i++) {
-            System.out.println(parentNode[i]);
-        }
+        process();
+        output();
     }
 }
