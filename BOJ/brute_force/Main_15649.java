fcaa47ad5a084400e26221334717911cb4904c5b
jjb8966 <jjb8966@naver.com>
2022-09-21 15:38:26 +0900

    15649번 복습

--- BOJ/brute_force/Main_15649.java
+++ BOJ/brute_force/Main_15649.java
@@ -1,6 +1,9 @@
 package brute_force;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 중복x, 순서o -> 시간복잡도 : O(N P M) = N! / (N-M)!
 public class Main_15649 {
@@ -8,45 +11,55 @@ public class Main_15649 {
     private static StringBuilder sb = new StringBuilder();
     private static int maxNumber;
     private static int maxDigit;
-    private static int[] values;
-    private static int[] used;
+    private static int[] result;
+    private static boolean[] used;
 
-    private static void input() {
-        Scanner sc = new Scanner(System.in);
-        maxNumber = sc.nextInt();
-        maxDigit = sc.nextInt();
-        used = new int[maxNumber + 1];        //1부터 N까지의 숫자중 사용된 것이 있으면 1, 없으면 0을 저장
-        values = new int[maxDigit + 1];
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+        output();
     }
 
-    public static void recurrenceFunction(int startDigit) {
-        // 1. 탐색이 끝난 경우
-        if (startDigit == maxDigit + 1) {
-            for (int digit = 1; digit <= maxDigit; digit++) {
-                sb.append(values[digit] + " ");     // 1번째 자리값 + 2번째 자리값 + ... + M번째 자리값
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        maxNumber = Integer.parseInt(st.nextToken());
+        maxDigit = Integer.parseInt(st.nextToken());
+
+        result = new int[maxDigit + 1];     // 1 ~ maxDigit
+        used = new boolean[maxNumber + 1];
+    }
+
+    private static void process() {
+        bruteForce(1);
+    }
+
+    private static void bruteForce(int digit) {
+        if (digit == maxDigit + 1) {
+            for (int i = 1; i <= maxDigit; i++) {
+                sb.append(result[i]).append(" ");
             }
 
             sb.append("\n");
-        } else {        // 2. 탐색이 남은 경우
-            for (int candidate = 1; candidate <= maxNumber; candidate++) {
-                if (used[candidate] == 1) {     // candidate 가 사용된 적이 있는 숫자이면 pass
+        } else {
+            for (int currentNumber = 1; currentNumber <= maxNumber; currentNumber++) {
+                if (used[currentNumber]) {
                     continue;
                 }
 
-                values[startDigit] = candidate;
-                used[candidate] = 1;
+                result[digit] = currentNumber;
+                used[currentNumber] = true;
 
-                recurrenceFunction(startDigit + 1);  // 마지막 재귀함수가 호출되면 1 과정을 통해 결과가 저장됨
+                bruteForce(digit + 1);
 
-                values[startDigit] = 0;      // 마지막 재귀함수가 호출된 후 반드시 초기화를 해줘야함
-                used[candidate] = 0;
+                used[currentNumber] = false;
             }
         }
     }
 
-    public static void main(String[] args) {
-        input();
-        recurrenceFunction(1);      //1번째 자리부터 옳바른 원소를 고르는 함수
+    private static void output() {
         System.out.println(sb);
     }
+
 }
