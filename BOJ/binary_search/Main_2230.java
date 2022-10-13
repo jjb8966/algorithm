96e87decf187afa2d22d03714fd171354c5a24ad
jjb8966 <jjb8966@naver.com>
2022-10-13 10:05:54 +0900

    백준 투 포인터 - 2230번 풀이

---
+++ BOJ/binary_search/Main_2230.java
@@ -0,0 +1,62 @@
+package binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2230 {
+
+    private static int n;
+    private static int m;
+    private static int[] numbers;
+    private static int minimumGap = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+        output();
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+    }
+
+    private static void process() {
+        int left = 0;
+        int right = 1;
+
+        Arrays.sort(numbers);
+
+        while (right < n) {
+            if (numbers[right] - numbers[left] >= m) {
+                if (numbers[right] - numbers[left] < minimumGap) {
+                    minimumGap = numbers[right] - numbers[left];
+                }
+
+                left++;
+                right = left + 1;
+            } else {
+                right++;
+            }
+        }
+    }
+
+    private static void output() {
+        System.out.println(minimumGap);
+    }
+
+}
