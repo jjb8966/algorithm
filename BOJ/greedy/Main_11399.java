ea668262551683fe623f8a664963e232eadec1f5
jjb8966 <jjb8966@naver.com>
2023-01-26 20:58:22 +0900

    백준 그리디 - 11399번 풀이

---
+++ BOJ/greedy/Main_11399.java
@@ -0,0 +1,52 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11399 {
+
+    static int numberOfPeople;
+    static int result;
+    static int[] times;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+        output();
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        numberOfPeople = Integer.parseInt(br.readLine());
+
+        times = new int[numberOfPeople];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < times.length; i++) {
+            int time = Integer.parseInt(st.nextToken());
+
+            times[i] = time;
+        }
+    }
+
+    private static void process() {
+        int currentTime = 0;
+
+        Arrays.sort(times);
+
+        for (int i = 0; i < times.length; i++) {
+            currentTime += times[i];
+            result += currentTime;
+        }
+    }
+
+    private static void output() {
+        System.out.println(result);
+    }
+
+}
