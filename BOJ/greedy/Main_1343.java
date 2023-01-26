8537fdf5bff9f54c3c1f90702c1deee7c5312dc7
jjb8966 <jjb8966@naver.com>
2023-01-26 18:19:17 +0900

    백준 그리디 - 1343번 풀이

--- BOJ/greedy/Main_1343.java
+++ BOJ/greedy/Main_1343.java
@@ -1,35 +1,34 @@
 package greedy;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main_1343 {
 
-    private static final String POLYOMINO_A = "AAAA";
-    private static final String POLYOMINO_B = "BB";
-    private static String board;
-
-    private static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        board = br.readLine();
-    }
-
-    private static void process() {
-        String temp = board.replace("XXXX", POLYOMINO_A);
-        String result = temp.replace("XX", POLYOMINO_B);
-
-        if (result.contains("X")) {
-            result = "-1";
-        }
-
-        System.out.println(result);
-    }
+    static String exp;
 
     public static void main(String[] args) throws IOException {
         input();
         process();
+        output();
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        exp = br.readLine();
+    }
+
+    private static void process() {
+        exp = exp.replace("XXXX", "AAAA");
+        exp = exp.replace("XX", "BB");
+
+        if (exp.contains("X")) {
+            exp = "-1";
+        }
+    }
+
+    private static void output() {
+        System.out.println(exp);
     }
 
 }
