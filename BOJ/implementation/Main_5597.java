010a8395e23a0ebc165da293b34f45dc612ee434
jjb8966 <jjb8966@naver.com>
2023-02-13 10:44:30 +0900

    백준 구현 - 5597번 풀이

--- BOJ/implementation/Main_5597.java
+++ BOJ/implementation/Main_5597.java
@@ -1,2 +1,42 @@
-package implementation;public class Main_5597 {
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class Main_5597 {
+
+    static Set<Integer> numbers = new HashSet<>();
+    static List<Integer> submits = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+        output();
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 28; i++) {
+            submits.add(Integer.valueOf(br.readLine()));
+        }
+    }
+
+    private static void process() {
+        IntStream.range(1, 31)
+                .forEach(n -> numbers.add(n));
+
+        submits.forEach(numbers::remove);
+    }
+
+    private static void output() {
+        numbers.stream().forEach(n -> System.out.println(n));
+    }
+
 }
