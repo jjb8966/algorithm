4378123fe63eda9986ea29b9934c0ea2c5e1539e
jjb8966 <jjb8966@naver.com>
2023-03-07 14:51:40 +0900

    백준 그래프탐색 - 16234번 복습

--- BOJ/src/main/java/graph_search/Main_16234.java
+++ BOJ/src/main/java/graph_search/Main_16234.java
@@ -9,15 +9,12 @@ import java.util.StringTokenizer;
 
 public class Main_16234 {
 
-    static int mapSize;
+    static int size;
     static int minGap;
     static int maxGap;
-    static int sum;
-    static int count;
-    static int day;
+    static int result;
     static int[][] map;
-    static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    static boolean move = true;
+    static int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
@@ -31,73 +28,56 @@ public class Main_16234 {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        mapSize = Integer.parseInt(st.nextToken());
+        size = Integer.parseInt(st.nextToken());
         minGap = Integer.parseInt(st.nextToken());
         maxGap = Integer.parseInt(st.nextToken());
 
-        map = new int[mapSize][mapSize];
+        map = new int[size][size];
 
-        for (int y = 0; y < mapSize; y++) {
+        for (int y = 0; y < size; y++) {
             st = new StringTokenizer(br.readLine());
 
-            for (int x = 0; x < mapSize; x++) {
+            for (int x = 0; x < size; x++) {
                 map[x][y] = Integer.parseInt(st.nextToken());
             }
         }
-
     }
 
     private static void process() {
-        while (true) {
-            move = false;
-            visited = new boolean[mapSize][mapSize];
+        int day = 0;
 
-            for (int y = 0; y < mapSize; y++) {
-                for (int x = 0; x < mapSize; x++) {
+        while (move()) {
+            day++;
+            visited = new boolean[size][size];
+
+            for (int y = 0; y < size; y++) {
+                for (int x = 0; x < size; x++) {
                     if (visited[x][y]) {
                         continue;
                     }
 
-                    sum = 0;
-                    count = 0;
-                    Queue<Integer> queue = new LinkedList<>();
+//                    bfs(x, y);
 
-                    dfs(x, y, queue);
-
-                    if (count == 1) {
-                        continue;
-                    }
-
-                    while (!queue.isEmpty()) {
-                        Integer changeX = queue.poll();
-                        Integer changeY = queue.poll();
-
-                        map[changeX][changeY] = sum / count;
-                    }
+                    Queue<Integer> connected = new LinkedList<>();
+                    dfs(x, y, connected);
+                    updateMap(connected);
                 }
             }
-
-            if (!move) {
-                break;
-            }
-
-            day++;
         }
+
+        result = day;
     }
 
-    private static void dfs(int x, int y, Queue<Integer> queue) {
+    private static void dfs(int x, int y, Queue<Integer> connected) {
         visited[x][y] = true;
-        queue.offer(x);
-        queue.offer(y);
-        sum += map[x][y];
-        count++;
+        connected.offer(x);
+        connected.offer(y);
 
         for (int dir = 0; dir < 4; dir++) {
             int newX = x + direction[dir][0];
             int newY = y + direction[dir][1];
-            int gap;
 
-            if (newX < 0 || newY < 0 || newX >= mapSize || newY >= mapSize) {
+            if (newX < 0 || newY < 0 || newX >= size || newY >= size) {
                 continue;
             }
 
@@ -105,19 +85,120 @@ public class Main_16234 {
                 continue;
             }
 
-            gap = Math.abs(map[x][y] - map[newX][newY]);
+            int absGap = Math.abs(map[newX][newY] - map[x][y]);
 
-            if (gap < minGap || gap > maxGap) {
+            if (absGap < minGap || absGap > maxGap) {
                 continue;
             }
 
-            move = true;
-            dfs(newX, newY, queue);
+            dfs(newX, newY, connected);
         }
     }
 
+    private static void updateMap(Queue<Integer> connected) {
+        int sum = 0;
+        int count = 0;
+        Queue<Integer> copy = new LinkedList<>(connected);
+
+        while (!connected.isEmpty()) {
+            Integer x = connected.poll();
+            Integer y = connected.poll();
+
+            sum += map[x][y];
+            count++;
+        }
+
+        while (!copy.isEmpty()) {
+            Integer x = copy.poll();
+            Integer y = copy.poll();
+
+            map[x][y] = sum / count;
+        }
+    }
+
+    private static void bfs(int startX, int startY) {
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> connected = new LinkedList<>();
+        int sum;
+        int count;
+
+        sum = map[startX][startY];
+        count = 1;
+        connected.offer(startX);
+        connected.offer(startY);
+
+        visited[startX][startY] = true;
+        queue.offer(startX);
+        queue.offer(startY);
+
+        while (!queue.isEmpty()) {
+            Integer x = queue.poll();
+            Integer y = queue.poll();
+
+            for (int dir = 0; dir < 4; dir++) {
+                int newX = x + direction[dir][0];
+                int newY = y + direction[dir][1];
+
+                if (newX < 0 || newY < 0 || newX >= size || newY >= size) {
+                    continue;
+                }
+
+                if (visited[newX][newY]) {
+                    continue;
+                }
+
+                int absGap = Math.abs(map[newX][newY] - map[x][y]);
+
+                if (absGap < minGap || absGap > maxGap) {
+                    continue;
+                }
+
+                sum += map[newX][newY];
+                count++;
+                connected.offer(newX);
+                connected.offer(newY);
+
+                visited[newX][newY] = true;
+                queue.offer(newX);
+                queue.offer(newY);
+            }
+        }
+
+        int value = sum / count;
+
+        while (!connected.isEmpty()) {
+            Integer x = connected.poll();
+            Integer y = connected.poll();
+
+            map[x][y] = value;
+        }
+    }
+
+    private static boolean move() {
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                for (int dir = 0; dir < 4; dir++) {
+                    int newX = x + direction[dir][0];
+                    int newY = y + direction[dir][1];
+
+                    if (newX < 0 || newY < 0 || newX >= size || newY >= size) {
+                        continue;
+                    }
+
+                    int absGap = Math.abs(map[newX][newY] - map[x][y]);
+
+                    if (absGap >= minGap && absGap <= maxGap) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
     private static void output() {
-        System.out.println(day);
+        System.out.println(result);
     }
 
 }
