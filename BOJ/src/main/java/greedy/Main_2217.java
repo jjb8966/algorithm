3d60c9da659146ef8ec49eed23ea98fa5d087bd4
jjb8966 <jjb8966@naver.com>
2023-03-07 15:55:51 +0900

    백준 그리디 - 2217번 복습

--- BOJ/src/main/java/greedy/Main_2217.java
+++ BOJ/src/main/java/greedy/Main_2217.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class Main_2217 {
@@ -31,7 +30,6 @@ public class Main_2217 {
     }
 
     private static void process() {
-        result = Integer.MIN_VALUE;
         Arrays.sort(lopes, Comparator.reverseOrder());
 
         for (int index = 0; index < lopes.length; index++) {
