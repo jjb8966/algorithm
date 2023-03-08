94391d975fe97f8adaf7f1543c8425877d2776b6
jjb8966 <jjb8966@naver.com>
2023-03-08 09:47:45 +0900

    백준 백트랙킹 - 14888번 복습

--- BOJ/src/main/java/backtracking/Main_14888.java
+++ BOJ/src/main/java/backtracking/Main_14888.java
@@ -11,7 +11,7 @@ public class Main_14888 {
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
     static int[] sequence;
-    static int[] operations = new int[4];
+    static int[] numberOfOperation = new int[4];
 
     public static void main(String[] args) throws IOException {
         input();
@@ -34,16 +34,72 @@ public class Main_14888 {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operations[i] = Integer.parseInt(st.nextToken());
+            numberOfOperation[i] = Integer.parseInt(st.nextToken());
         }
     }
 
     private static void process() {
-        dfs(1, sequence[0]);
+        backtracking(1, sequence[0]);
+//        backtracking2(0, new int[lengthOfSequence - 1]);
     }
 
-    private static void dfs(int nextIndex, int sum) {
-        if (nextIndex == lengthOfSequence) {
+    private static void backtracking(int index, int sum) {
+        if (index == lengthOfSequence) {
+            min = Math.min(min, sum);
+            max = Math.max(max, sum);
+
+            return;
+        }
+
+        for (int op = 0; op < 4; op++) {
+            if (numberOfOperation[op] <= 0) {
+                continue;
+            }
+
+            numberOfOperation[op]--;
+
+            if (op == 0) {
+                backtracking(index + 1, sum + sequence[index]);
+            }
+
+            if (op == 1) {
+                backtracking(index + 1, sum - sequence[index]);
+            }
+
+            if (op == 2) {
+                backtracking(index + 1, sum * sequence[index]);
+            }
+
+            if (op == 3) {
+                backtracking(index + 1, sum / sequence[index]);
+            }
+
+            numberOfOperation[op]++;
+        }
+    }
+
+    private static void backtracking2(int index, int[] opCandidate) {
+        if (index == lengthOfSequence - 1) {
+            int sum = sequence[0];
+
+            for (int i = 0; i < opCandidate.length; i++) {
+                if (opCandidate[i] == 0) {
+                    sum += sequence[i + 1];
+                }
+
+                if (opCandidate[i] == 1) {
+                    sum -= sequence[i + 1];
+                }
+
+                if (opCandidate[i] == 2) {
+                    sum *= sequence[i + 1];
+                }
+
+                if (opCandidate[i] == 3) {
+                    sum /= sequence[i + 1];
+                }
+            }
+
             max = Math.max(max, sum);
             min = Math.min(min, sum);
 
@@ -51,29 +107,15 @@ public class Main_14888 {
         }
 
         for (int op = 0; op < 4; op++) {
-            if (operations[op] <= 0) {
-                continue;
+            if (numberOfOperation[op] > 0) {
+                numberOfOperation[op]--;
+                opCandidate[index] = op;
+
+                backtracking2(index + 1, opCandidate);
+
+                opCandidate[index] = 0;
+                numberOfOperation[op]++;
             }
-
-            operations[op]--;
-
-            if (op == 0) {
-                dfs(nextIndex + 1, sum + sequence[nextIndex]);
-            }
-
-            if (op == 1) {
-                dfs(nextIndex + 1, sum - sequence[nextIndex]);
-            }
-
-            if (op == 2) {
-                dfs(nextIndex + 1, sum * sequence[nextIndex]);
-            }
-
-            if (op == 3) {
-                dfs(nextIndex + 1, sum / sequence[nextIndex]);
-            }
-
-            operations[op]++;
         }
     }
 
