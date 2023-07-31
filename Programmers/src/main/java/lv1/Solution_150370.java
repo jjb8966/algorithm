2ebdedd736a3f0971c46db480a3da9dc8e939d54
jjb8966 <jjb8966@naver.com>
2023-07-31 10:11:27 +0900

    프로그래머스 lv1 - 개인정보 수집 유효기간

---
+++ Programmers/src/main/java/lv1/Solution_150370.java
@@ -0,0 +1,22 @@
+package lv1;
+
+public class Solution_150370 {
+
+    public static void main(String[] args) {
+        Solution_150370 prob = new Solution_150370();
+
+        System.out.println(prob.solution(
+                "2022.05.19",
+                new String[]{"A 6", "B 12", "C 3"},
+                new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"}
+        ));
+    }
+
+    private boolean solution(String today, String[] terms, String[] privacies) {
+        String test = "a.b.c";
+        String[] split = test.split("\\.");
+
+        return false;
+    }
+
+}
