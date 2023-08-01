128711c8e7aee5681fcd9fc58a95b282b158b87b
jjb8966 <jjb8966@naver.com>
2023-08-01 10:07:54 +0900

    프로그래머스 lv1 - 개인정보 수집 유효기간

--- Programmers/src/main/java/lv1/Solution_150370.java
+++ Programmers/src/main/java/lv1/Solution_150370.java
@@ -1,5 +1,9 @@
 package lv1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Solution_150370 {
 
     public static void main(String[] args) {
@@ -12,11 +16,60 @@ public class Solution_150370 {
         ));
     }
 
-    private boolean solution(String today, String[] terms, String[] privacies) {
-        String test = "a.b.c";
-        String[] split = test.split("\\.");
+    private int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> result = new ArrayList<>();
 
-        return false;
+        // 1. today 연/월/일 분리
+        String[] todayInfo = today.split("\\.");
+        int currentYear = Integer.parseInt(todayInfo[0]);
+        int currentMonth = Integer.parseInt(todayInfo[1]);
+        int currentDay = Integer.parseInt(todayInfo[2]);
+
+        // 2. terms -> map
+        HashMap<String, Integer> termToExpirationDateMap = new HashMap<>();
+
+        for (String exp : terms) {
+            String[] termInfo = exp.split(" ");
+            String term = termInfo[0];
+            int duration = Integer.parseInt(termInfo[1]);
+
+            termToExpirationDateMap.put(term, duration);
+        }
+
+        for (int index = 1; index < privacies.length; index++) {
+            String exp = privacies[index];
+
+            // 3.1 privacies -> 날짜/약관 분리
+            String[] privacyInfo = exp.split(" ");
+            String date = privacyInfo[0];
+            String[] dateInfo = date.split("\\.");
+            int year = Integer.parseInt(dateInfo[0]);
+            int month = Integer.parseInt(dateInfo[1]);
+            int day = Integer.parseInt(dateInfo[2]);
+
+            String term = privacyInfo[1];
+
+            // 3.1 약관 만료일 구하기
+            Integer duration = termToExpirationDateMap.get(term);
+
+            // 3.2 만료 날짜 구하기
+            int sumMonth = month + duration;
+
+            if (sumMonth > 12) {
+                year++;
+            }
+
+            // 3.3 현재 날짜와 비교해서 만료되었으면 결과에 추가
+            int calcDay = currentDay - day;
+
+            if (calcDay < 0) {
+
+            }
+
+        }
+
+        return result.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
-
 }
