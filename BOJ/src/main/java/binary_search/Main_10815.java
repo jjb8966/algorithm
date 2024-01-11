e3a7e4d27f45c038978a34ccca385b0e7d0adc7a
jjb8966 <jjb8966@naver.com>
2024-01-11 20:20:21 +0900

    백준 이분탐색 - [복습] 10815번: 숫자 카드

--- BOJ/src/main/java/binary_search/Main_10815.java
+++ BOJ/src/main/java/binary_search/Main_10815.java
@@ -8,72 +8,70 @@ import java.util.StringTokenizer;
 
 public class Main_10815 {
 
-    static int numberOfCards;
-    static int numberOfSearches;
-    static int[] cards;
-    static int[] searches;
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
-    }
-
-    private static void input() throws IOException {
+        // init
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        numberOfCards = Integer.parseInt(br.readLine());
-
-        cards = new int[numberOfCards];
+        int numberOfCards = Integer.parseInt(br.readLine());
+//        Set<Object> numbers = new HashSet<>();
+        int[] numbers = new int[numberOfCards];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < numberOfCards; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+//            numbers.add(Integer.parseInt(st.nextToken()));
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        numberOfSearches = Integer.parseInt(br.readLine());
-
-        searches = new int[numberOfSearches];
+        int numberOfSearches = Integer.parseInt(br.readLine());
+        int[] searches = new int[numberOfSearches];
+        int[] result = new int[numberOfSearches];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < numberOfSearches; i++) {
             searches[i] = Integer.parseInt(st.nextToken());
         }
-    }
 
-    private static void process() {
-        Arrays.sort(cards);
+        // process
+        // sol1) set 사용해서 풀기
+//        for (int index = 0; index < numberOfSearches; index++) {
+//            if (numbers.contains(searches[index])) {
+//                result[index] = 1;
+//            } else {
+//                result[index] = 0;
+//            }
+//        }
 
+        // sol2) 이분탐색
+        Arrays.sort(numbers);
         for (int index = 0; index < numberOfSearches; index++) {
-            binarySearch(0, numberOfCards - 1, index);
-        }
-    }
-
-    private static void binarySearch(int min, int max, int index) {
-        while (min <= max) {
-            int mid = (min + max) / 2;
-
-            if (cards[mid] == searches[index]) {
-                sb.append(1).append(" ");
-                return;
-            }
-
-            if (cards[mid] < searches[index]) {
-                min = mid + 1;
-            }
-
-            if (cards[mid] > searches[index]) {
-                max = mid - 1;
-            }
+            result[index] = binarySearch(0, numberOfCards - 1, searches[index], numbers);
         }
 
-        sb.append(0).append(" ");
-    }
+        // output
+        StringBuilder sb = new StringBuilder();
+        for (int number : result) {
+            sb.append(number).append(" ");
+        }
 
-    private static void output() {
         System.out.println(sb);
     }
 
+    private static int binarySearch(int min, int max, int target, int[] numbers) {
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (numbers[mid] == target) {
+                return 1;
+            }
+
+            if (numbers[mid] > target) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        return 0;
+    }
 }
