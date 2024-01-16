79de97b4f4816641632ea561aa380d2eb636c972
jjb8966 <jjb8966@naver.com>
2024-01-16 21:18:03 +0900

    백준 이분탐색 - [복습] 2805번: 나무 자르기

--- BOJ/src/main/java/binary_search/Main_2805.java
+++ BOJ/src/main/java/binary_search/Main_2805.java
@@ -1,74 +1,67 @@
 package binary_search;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main_2805 {
 
-    static int numberOfTrees;
-    static int targetHeight;
-    static long result;
-    static int[] trees;
-
     public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
-    }
-
-    private static void input() throws IOException {
+        // init
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        int numberOfTree = 0;
+        int target = 0;
+        int result = 0;
+        int maxHeight = 0;
+        int[] trees;
 
         st = new StringTokenizer(br.readLine());
-        numberOfTrees = Integer.parseInt(st.nextToken());
-        targetHeight = Integer.parseInt(st.nextToken());
+        numberOfTree = Integer.parseInt(st.nextToken());
+        target = Integer.parseInt(st.nextToken());
 
-        trees = new int[numberOfTrees];
-
+        trees = new int[numberOfTree];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < numberOfTrees; i++) {
+        for (int i = 0; i < numberOfTree; i++) {
             trees[i] = Integer.parseInt(st.nextToken());
+            maxHeight = Math.max(maxHeight, trees[i]);
         }
+
+        // process
+        result = binarySearch(0, maxHeight, target, trees);
+
+        // output
+        System.out.println(result);
     }
 
-    private static void process() {
-        int max = Arrays.stream(trees)
-                .max()
-                .getAsInt();
+    private static int binarySearch(int min, int max, int target, int[] trees) {
+        int result = 0;
+        int mid = 0;
 
-        binarySearch(0, max);
-    }
-
-    private static void binarySearch(int min, int max) {
         while (min <= max) {
-            int mid = (min + max) / 2;
+            mid = (min + max) / 2;
 
-            if (isPossible(mid)) {
+            if (isPossible(mid, target, trees)) {
                 result = mid;
                 min = mid + 1;
             } else {
                 max = mid - 1;
             }
         }
+
+        return result;
     }
 
-    private static boolean isPossible(int height) {
+    private static boolean isPossible(int mid, int target, int[] trees) {
         long sum = 0;
 
-        for (int i = 0; i < numberOfTrees; i++) {
-            if (trees[i] > height) {
-                sum += trees[i] - height;
+        for (int treeHeight : trees) {
+            int getTree = treeHeight - mid;
+
+            if (getTree > 0) {
+                sum += getTree;
             }
         }
 
-        return sum >= targetHeight;
-    }
-
-    private static void output() {
-        System.out.println(result);
+        return sum >= target;
     }
 }
