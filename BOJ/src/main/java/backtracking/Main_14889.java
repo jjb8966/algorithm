b963d9b62aa0a6ef654258738757679281a33628
jjb8966 <jjb8966@naver.com>
2024-01-23 00:38:44 +0900

    백준 이분탐색 - [복습] 14889번: 스타트와 링크

--- BOJ/src/main/java/backtracking/Main_14889.java
+++ BOJ/src/main/java/backtracking/Main_14889.java
@@ -7,79 +7,76 @@ import java.util.StringTokenizer;
 
 public class Main_14889 {
 
-    static int numberOfPeople;
-    static int min = Integer.MAX_VALUE;
-    static int[][] stats;
-    static boolean[] isTeamStart;
-
     public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
-    }
-
-    private static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // init
+        int numberOfPeople = 0;
+        int minimumGap = 0;
+        int[][] stat;
+        boolean[] isTeamStart;
         StringTokenizer st;
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         numberOfPeople = Integer.parseInt(br.readLine());
 
-        stats = new int[numberOfPeople][numberOfPeople];
+        stat = new int[numberOfPeople][numberOfPeople];
         isTeamStart = new boolean[numberOfPeople];
 
         for (int y = 0; y < numberOfPeople; y++) {
             st = new StringTokenizer(br.readLine());
 
             for (int x = 0; x < numberOfPeople; x++) {
-                stats[x][y] = Integer.parseInt(st.nextToken());
+                stat[x][y] = Integer.parseInt(st.nextToken());
             }
         }
+
+        // process
+        minimumGap = backtracking(0, 0, numberOfPeople, stat, isTeamStart);
+
+        // output
+        System.out.println(minimumGap);
     }
 
-    private static void process() {
-        backtracking(0, 0);
+    private static int backtracking(int memberIndex,
+                                    int countOfTeamStart,
+                                    int numberOfPeople,
+                                    int[][] stat,
+                                    boolean[] isTeamStart) {
+        int minimumGap = Integer.MAX_VALUE;
+
+        if (countOfTeamStart == numberOfPeople / 2) {
+            return calculateGap(numberOfPeople, stat, isTeamStart);
+        }
+
+        for (int index = memberIndex; index < numberOfPeople; index++) {
+            isTeamStart[index] = true;
+
+            int gap = backtracking(index + 1, countOfTeamStart + 1, numberOfPeople, stat, isTeamStart);
+            minimumGap = Math.min(minimumGap, gap);
+
+            isTeamStart[index] = false;
+        }
+
+        return minimumGap;
     }
 
-    private static void backtracking(int member, int count) {
-        if (count == numberOfPeople / 2) {
-            int sumOfStart = 0;
-            int sumOfLink = 0;
+    private static int calculateGap(int numberOfPeople,
+                                    int[][] stat,
+                                    boolean[] isTeamStart) {
+        int sumOfStart = 0;
+        int sumOfLink = 0;
 
-            for (int y = 0; y < numberOfPeople; y++) {
-                for (int x = 0; x < numberOfPeople; x++) {
-                    if (x == y) {
-                        continue;
-                    }
+        for (int y = 0; y < numberOfPeople; y++) {
+            for (int x = 0; x < numberOfPeople; x++) {
+                if (isTeamStart[x] && isTeamStart[y]) {
+                    sumOfStart += stat[x][y];
+                }
 
-                    if (isTeamStart[x] && isTeamStart[y]) {
-                        sumOfStart += stats[x][y];
-                    }
-
-                    if (!isTeamStart[x] && !isTeamStart[y]) {
-                        sumOfLink += stats[x][y];
-                    }
+                if (!isTeamStart[x] && !isTeamStart[y]) {
+                    sumOfLink += stat[x][y];
                 }
             }
-
-            int gap = Math.abs(sumOfStart - sumOfLink);
-            min = Math.min(min, gap);
-
-            return;
         }
 
-        if (member == numberOfPeople) {
-            return;
-        }
-
-        backtracking(member + 1, count);
-
-        isTeamStart[member] = true;
-        backtracking(member + 1, count + 1);
-        isTeamStart[member] = false;
+        return Math.abs(sumOfStart - sumOfLink);
     }
-
-    private static void output() {
-        System.out.println(min);
-    }
-
 }
