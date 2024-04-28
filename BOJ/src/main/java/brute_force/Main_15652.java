91c202c2fe3bbaef669270c987d792101409fccf
jjb8966 <jjb8966@naver.com>
2024-04-28 23:47:34 +0900

    백준 완전탐색 - [복습] 15652번: N과 M (4)

--- BOJ/src/main/java/brute_force/Main_15652.java
+++ BOJ/src/main/java/brute_force/Main_15652.java
@@ -7,18 +7,11 @@ import java.util.StringTokenizer;
 
 public class Main_15652 {
 
-    private static int N;
-    private static int M;
-    private static int[] result;
-    private static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
-    }
-
-    private static void input() throws IOException {
+        // init
+        int N;
+        int M;
+        int[] result;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -26,38 +19,33 @@ public class Main_15652 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        result = new int[M + 1];
+        result = new int[M];
+
+        // process
+        StringBuilder sb = new StringBuilder();
+        bruteForce(N, M, result, sb, 0);
+
+        // output
+        System.out.println(sb);
     }
 
-    private static void process() {
-        bruteForce(1);
-    }
-
-    private static void bruteForce(int digit) {
-        if (digit == M + 1) {
-            for (int index = 1; index <= M; index++) {
-                sb.append(result[index]).append(" ");
+    static void bruteForce(int N, int M, int[] result, StringBuilder sb, int digit) {
+        if (digit == M) {
+            for (int number : result) {
+                sb.append(number).append(" ");
             }
-
             sb.append('\n');
-
             return;
         }
 
         for (int candidate = 1; candidate <= N; candidate++) {
-            if (result[digit - 1] > candidate) {
+            if (result[digit] > candidate) {
                 continue;
             }
 
             result[digit] = candidate;
-
-            bruteForce(digit + 1);
+            bruteForce(N, M, result, sb, digit + 1);
         }
-
-    }
-
-    private static void output() {
-        System.out.println(sb);
     }
 
 }
