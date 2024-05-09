b3777e4f09015724bf67c339f0bbb948e16a6543
jjb8966 <jjb8966@naver.com>
2024-05-10 00:49:26 +0900

    백준 그래프탐색 - [복습] 2606번: 바이러스

--- BOJ/src/main/java/graph_search/Main_2606.java
+++ BOJ/src/main/java/graph_search/Main_2606.java
@@ -6,77 +6,77 @@ import java.util.*;
 public class Main_2606 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int numberOfComputer = Integer.parseInt(br.readLine());
-        int numberOfPair = Integer.parseInt(br.readLine());
-        boolean[] visited = new boolean[numberOfComputer + 1];
-        List[] adjacenyList = new List[numberOfComputer + 1];
-
         // init
-        for (int number = 1; number <= numberOfComputer; number++) {
-            adjacenyList[number] = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int numberOfComputer = Integer.parseInt(br.readLine());
+        int numberOfConnect = Integer.parseInt(br.readLine());
+
+        List<Integer>[] adjacencyList = new List[numberOfComputer + 1];
+        boolean[] isVisited = new boolean[numberOfComputer + 1];
+
+        for (int i = 1; i <= numberOfComputer; i++) {
+            adjacencyList[i] = new ArrayList<>();
         }
 
-        for (int number = 1; number <= numberOfPair; number++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < numberOfConnect; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            int firstVertex = Integer.parseInt(st.nextToken());
-            int secondVertex = Integer.parseInt(st.nextToken());
-
-            adjacenyList[firstVertex].add(secondVertex);
-            adjacenyList[secondVertex].add(firstVertex);
+            adjacencyList[a].add(b);
+            adjacencyList[b].add(a);
         }
 
         // process
-        int result = dfs(1, visited, adjacenyList) - 1;
-//        int result = bfs(1, visited, adjacenyList);
+//        int result = dfs(1, isVisited, adjacencyList) - 1;
+        int result = bfs(1, isVisited, adjacencyList);
 
         // output
         System.out.println(result);
     }
 
     private static int dfs(int vertex,
-                           boolean[] visited,
-                           List<Integer>[] adjacenyList) {
-        int count = 1;
+                           boolean[] isVisited,
+                           List<Integer>[] adjacencyList) {
+        isVisited[vertex] = true;
+        int result = 1;
 
-        visited[vertex] = true;
-
-        for (Integer connectVertex : adjacenyList[vertex]) {
-            if (visited[connectVertex]) {
+        for (Integer connectVertex : adjacencyList[vertex]) {
+            if (isVisited[connectVertex]) {
                 continue;
             }
 
-            count += dfs(connectVertex, visited, adjacenyList);
-        }
-
-        return count;
-    }
-
-    private static int bfs(int start,
-                           boolean[] visited,
-                           List<Integer>[] adjacenyList) {
-        Queue<Integer> queue = new LinkedList<>();
-        int result = 0;
-
-        visited[start] = true;
-        queue.offer(start);
-
-        while (!queue.isEmpty()) {
-            Integer vertex = queue.poll();
-
-            for (Integer connectVertex : adjacenyList[vertex]) {
-                if (visited[connectVertex]) {
-                    continue;
-                }
-
-                visited[connectVertex] = true;
-                result++;
-                queue.add(connectVertex);
-            }
+            result += dfs(connectVertex, isVisited, adjacencyList);
         }
 
         return result;
     }
 
+    private static int bfs(int start,
+                           boolean[] isVisited,
+                           List<Integer>[] adjacencyList) {
+        Queue<Integer> queue = new LinkedList();
+        int result = 0;
+
+        isVisited[start] = true;
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            Integer vertex = queue.poll();
+
+            for (Integer connectVertex : adjacencyList[vertex]) {
+                if (isVisited[connectVertex]) {
+                    continue;
+                }
+
+                isVisited[connectVertex] = true;
+                queue.offer(connectVertex);
+                result++;
+            }
+        }
+
+        return result;
+    }
 }
