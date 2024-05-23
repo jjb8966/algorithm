79a60dc718388a4a6d9ef9fd452791e17b1782b0
jjb8966 <jjb8966@naver.com>
2024-05-23 23:47:21 +0900

    백준 그래프탐색 - [복습] 11726번: 2×n 타일링

--- BOJ/src/main/java/dynamic_programming/Main_11726.java
+++ BOJ/src/main/java/dynamic_programming/Main_11726.java
@@ -1,44 +1,28 @@
 package dynamic_programming;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main_11726 {
 
-    static int targetNumber;
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
-        input();
-        process();
-        output();
-    }
-
-    private static void input() throws IOException {
+        // init
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        targetNumber = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] result = new int[n + 1];
 
-        dp = new int[targetNumber + 1];
-    }
+        // process
+        result[1] = 1;
 
-    private static void process() {
-        if (targetNumber <= 3) {
-            dp = new int[3 + 1];
+        if (n > 1) {
+            result[2] = 2;
         }
 
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-
-        for (int number = 4; number <= targetNumber; number++) {
-            dp[number] = dp[number - 1] + dp[number - 2];
-            dp[number] %= 10_007;
+        for (int i = 3; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+            result[i] %= 10007;
         }
-    }
 
-    private static void output() {
-        System.out.println(dp[targetNumber]);
+        // output
+        System.out.println(result[n]);
     }
-
 }
